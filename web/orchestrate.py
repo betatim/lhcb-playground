d7f1046d2d20a3807a4a7093e5d26696db392d81
Tim Head <betatim@gmail.com>
2015-06-13 18:21:23 +0200

    Fetching gists, asynchronously, like a pro.

--- web/orchestrate.py
+++ web/orchestrate.py
@@ -1,16 +1,44 @@
+import os
+
+from concurrent.futures import ThreadPoolExecutor
+
+import tornado
+from tornado import gen, web
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url
 
+import github3
 
-class HelloHandler(RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+
+class GistHandler(RequestHandler):
+    @gen.coroutine
+    def get(self, gist_id=None):
+        # betatim/c4af33d04542f4306d38 -> c4af33d04542f4306d38
+        gist_id = gist_id.split("/")[-1]
+        gist = yield self.fetcher_pool.submit(github3.gist, gist_id)
+        
+        self.render("gist_display.html",
+                    gist_id=gist_id,
+                    description=gist.description,
+                    files=[f for f in gist.iter_files()])
+
+    @property
+    def fetcher_pool(self):
+        return self.settings['fetcher_pool']
 
 
 def make_app():
-    return Application([
-        url(r"/", HelloHandler),
-    ])
+    docker_host = os.environ.get('DOCKER_HOST', 'unix://var/run/docker.sock')
+    
+    handlers = [
+        # match /gist/betatim/c4af33d04542f4306d38 or /gist/c4af33d04542f4306d38
+        url(r"/gist/(\w+(?:/\w+)?)", GistHandler),
+    ]
+
+    settings = dict(fetcher_pool=ThreadPoolExecutor(4),
+                )
+    
+    return Application(handlers, **settings)
 
 def main():
     app = make_app()
